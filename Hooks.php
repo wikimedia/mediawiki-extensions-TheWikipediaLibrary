e3b8bbed1aae6798c71a87dedd1efda6fd99112b
Roan Kattouw <roan.kattouw@gmail.com>
2018-10-19 17:04:18 -0700

    Various fixes and cleanup
    - Convert to short array syntax
    - Use correct parameters for PageContentSaveComplete hook
    - Fix indentation
    
    Change-Id: I7d54a4848980c88ee752b4f5d7294ee2a67c0ec7

--- Hooks.php
+++ Hooks.php
@@ -41,48 +41,56 @@ class TheWikipediaLibraryHooks {
 	}
 
 	/**
-	 * Use this hook to remove feed links from the head
-	 * of the output.
+	 * Send a Wikipedia Library notification if the user has reached 6 months and 500 edits.
 	 *
-	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/AfterBuildFeedLinks
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/PageContentSaveComplete
 	 *
-	 * @param array &$feedLinks Added feed links to be
-	 *  outputted. You can remove them using unset(), and
-	 *  it's possible to add additional feed links. However,
-	 *  you should use the OutputPage::addFeedLink() method,
-	 *  instead.
+	 * @param WikiPage $wikiPage
+	 * @param User $user
+	 * @param Content $content
+	 * @param string $summary
+	 * @param bool $isMinor
+	 * @param bool $watch
+	 * @param string $section
+	 * @param int $flags
+	 * @param Revision $revision
+	 * @param Status &$status
+	 * @param int $baseRevId
+	 * @param int $undidRevId
 	 */
-	public static function onPageContentSaveComplete( WikiPage &$wikiPage, &$user, $content, $summary, $minoredit, $watchthis, $sectionanchor, &$flags, $revision, &$status ) {
-		$title = $wikiPage->getTitle();
+	public static function onPageContentSaveComplete(
+			WikiPage &$wikiPage, User &$user, Content $content, $summary, $isMinor, $watch, $section,
+			&$flags, Revision $revision, Status &$status, $baseRevId, $undidRevId
+	) {
 		global $wgTwlSendNotifications;
+		$title = $wikiPage->getTitle();
 
-			// if the feature flag is set to true, schedule a callable update.
-			if( $wgTwlSendNotifications ) {
-				// if the user has 500 edits and has been registered for 6 months give access
-				// to the-wikipedia-library database
-				DeferredUpdates::addCallableUpdate( function () use ( $user, $title ) {
-					global $wgTwlEditCount, $wgTwlRegistrationDays, $wgTwlRegistrationHours, $wgTwlRegistrationSeconds;
-					$twlRegistrationPeriod = $wgTwlRegistrationDays * $wgTwlRegistrationHours * $wgTwlRegistrationSeconds;
+		// if the feature flag is set to true, schedule a callable update.
+		if( $wgTwlSendNotifications ) {
+			// if the user has 500 edits and has been registered for 6 months give access
+			// to the-wikipedia-library database
+			DeferredUpdates::addCallableUpdate( function () use ( $user, $title ) {
+				global $wgTwlEditCount, $wgTwlRegistrationDays, $wgTwlRegistrationHours, $wgTwlRegistrationSeconds;
+				$twlRegistrationPeriod = $wgTwlRegistrationDays * $wgTwlRegistrationHours * $wgTwlRegistrationSeconds;
 
-					// notify the user only once about the twl-eligibility
-					$notificationMapper = new EchoNotificationMapper();
-					$notifications = $notificationMapper->fetchByUser( $user, 1, null, array( 'twl-eligible' ) );
-					if ( count( $notifications ) >= 1 ) {
-						return;
-					}
-					$registration_timestamp = wfTimestamp( TS_UNIX, $user->getRegistration() );
-					$lastedit_timestamp = wfTimestamp( TS_UNIX, wfTimestampNow( TS_UNIX ) );
-					$eligibility_period_timestamp = $lastedit_timestamp - $registration_timestamp;
-					if ( $user->getEditCount() >= $wgTwlEditCount && $eligibility_period_timestamp >= $twlRegistrationPeriod ) {
-						EchoEvent::create( array(
-								'type' => 'twl-eligible',
-								'agent' => $user,
-								// Wikipedia library eligiblity notification is sent to the agent
-								'extra' => array(
-									'notifyAgent' => true,
-							)
-						)
-					);
+				// notify the user only once about the twl-eligibility
+				$notificationMapper = new EchoNotificationMapper();
+				$notifications = $notificationMapper->fetchByUser( $user, 1, null, [ 'twl-eligible' ] );
+				if ( count( $notifications ) >= 1 ) {
+					return;
+				}
+				$registration_timestamp = wfTimestamp( TS_UNIX, $user->getRegistration() );
+				$lastedit_timestamp = wfTimestamp( TS_UNIX, wfTimestampNow( TS_UNIX ) );
+				$eligibility_period_timestamp = $lastedit_timestamp - $registration_timestamp;
+				if ( $user->getEditCount() >= $wgTwlEditCount && $eligibility_period_timestamp >= $twlRegistrationPeriod ) {
+					EchoEvent::create( [
+						'type' => 'twl-eligible',
+						'agent' => $user,
+						// Wikipedia library eligiblity notification is sent to the agent
+						'extra' => [
+							'notifyAgent' => true,
+						]
+					] );
 				}
 			} );
 		}
--- includes/TwlEligiblePresentationModel.php
+++ includes/TwlEligiblePresentationModel.php
@@ -14,32 +14,26 @@ class TwlEligiblePresentationModel extends EchoEventPresentationModel {
 		return $this->msg( 'notification-body-twl-eligiblity' );
 	}
 
-	/**
-	 * Array of primary link details, with possibly-relative URL & label.
-	 *
-	 * @return array|bool Array of link data, or false for no link:
-	 *                    ['url' => (string) url, 'label' => (string) link text (non-escaped)]
-	 */
 	public function getPrimaryLink() {
 		global $wgTwlUserPrimaryUrl;
-		return array(
+		return [
 			'url' => $wgTwlUserPrimaryUrl,
 			'label' => $this->msg( 'notification-twl-eligiblity-primarylink-text' )->text(),
-		);
+		];
 	}
 
 	public function getSecondaryLinks() {
-		return array( $this->getTwlSecondaryLink() );
+		return [ $this->getTwlSecondaryLink() ];
 	}
 
 	private function getTwlSecondaryLink() {
 		global $wgTwlUserSecondaryUrl;
-		return array(
+		return [
 			'url' => $wgTwlUserSecondaryUrl,
 			'label' => $this->msg( 'notification-twl-eligiblity-secondarylink-text' )->text(),
 			'description' => '',
 			'icon' => 'article',
 			'prioritized' => true,
-		);
+		];
 	}
 }
