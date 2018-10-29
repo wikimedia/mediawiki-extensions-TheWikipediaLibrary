0c8b6458a24ed4fd11b9d7a2a4389b9c871b2c7a
Roan Kattouw <roan.kattouw@gmail.com>
2018-10-29 16:43:42 -0700

    Enable phpcs and make pass
    Change-Id: I43ebb80371a59dd7b04460bf42b632ba94eb8e99

--- includes/TwlEligiblePresentationModel.php
+++ includes/TwlEligiblePresentationModel.php
@@ -2,18 +2,30 @@
 
 class TwlEligiblePresentationModel extends EchoEventPresentationModel {
 
+	/**
+	 * @inheritDoc
+	 */
 	public function getIconType() {
 		return 'twl-eligible';
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function getHeaderMessageKey() {
 		return 'notification-header-twl-eligiblity';
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function getBodyMessage() {
 		return $this->msg( 'notification-body-twl-eligiblity' );
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function getPrimaryLink() {
 		global $wgTwlUserPrimaryUrl;
 		return [
@@ -22,6 +34,9 @@ class TwlEligiblePresentationModel extends EchoEventPresentationModel {
 		];
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public function getSecondaryLinks() {
 		return [ $this->getTwlSecondaryLink() ];
 	}
