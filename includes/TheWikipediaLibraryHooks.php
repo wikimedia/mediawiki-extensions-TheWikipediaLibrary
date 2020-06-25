0dd780adf78e3497b52f76074b3db4c86476ef52
DannyS712 <DannyS712.enwiki@gmail.com>
2020-06-25 17:33:26 +0000

    Update hooks to use `PageSaveComplete` hook in MW 1.35+
    Bug: T250566
    Change-Id: I4232622cd016ddddaf85468feef42322350e247a

--- includes/TheWikipediaLibraryHooks.php
+++ includes/TheWikipediaLibraryHooks.php
@@ -12,6 +12,22 @@ use MediaWiki\MediaWikiServices;
 class TheWikipediaLibraryHooks {
 
 	/**
+	 * Callback on extension registration
+	 *
+	 * Register hooks based on version to keep support for mediawiki versions before 1.35
+	 */
+	public static function onRegistration() {
+		global $wgHooks;
+
+		if ( version_compare( MW_VERSION, '1.35', '>=' ) ) {
+			$wgHooks['PageSaveComplete'][] = 'TheWikipediaLibraryHooks::onPageSaveComplete';
+		} else {
+			$wgHooks['PageContentSaveComplete'][] = 'TheWikipediaLibraryHooks::onPageContentSaveComplete';
+		}
+	}
+
+
+	/**
 	 * Add The Wikipedia Library - eligibility events to Echo
 	 *
 	 * @param array &$notifications array of Echo notifications
@@ -54,6 +70,36 @@ class TheWikipediaLibraryHooks {
 	/**
 	 * Send a Wikipedia Library notification if the user has reached 6 months and 500 edits.
 	 *
+	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/PageSaveComplete
+	 *
+	 * @note paramaters include classes not available before 1.35, so for those typehints
+	 * are not used. The variable name reflects the class
+	 *
+	 * @param WikiPage $wikiPage
+	 * @param mixed $userIdentity
+	 * @param string $summary
+	 * @param int $flags
+	 * @param mixed $revisionRecord
+	 * @param mixed $editResult
+	 */
+	public static function onPageSaveComplete(
+		WikiPage $wikiPage,
+		$userIdentity,
+		string $summary,
+		int $flags,
+		$revisionRecord,
+		$editResult
+	) {
+		global $wgTwlSendNotifications;
+		if ( $wgTwlSendNotifications ) {
+			$user = User::newFromIdentity( $userIdentity );
+			self::maybeSendNotification( $user );
+		}
+	}
+
+	/**
+	 * Send a Wikipedia Library notification if the user has reached 6 months and 500 edits.
+	 *
 	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/PageContentSaveComplete
 	 *
 	 * @param WikiPage $wikiPage
@@ -75,38 +121,47 @@ class TheWikipediaLibraryHooks {
 	) {
 		global $wgTwlSendNotifications;
 		if ( $wgTwlSendNotifications ) {
-			// Send a notification if the user has at least $wgTwlEditCount edits and their account
-			// is at least $wgTwlRegistrationDays days old
-			DeferredUpdates::addCallableUpdate( function () use ( $user ) {
-				global $wgTwlEditCount, $wgTwlRegistrationDays;
-
-				// If we've already notified this user, don't notify them again
-				if ( $user->getOption( 'twl-notified' ) ) {
-					return;
-				}
-
-				$globalUser = CentralAuthUser::getInstance( $user );
-				if ( !$globalUser->isAttached() ) {
-					return;
-				}
-				$accountAge = (int)wfTimestamp( TS_UNIX ) -
-					(int)wfTimestamp( TS_UNIX, $globalUser->getRegistration() );
-				$minimumAge = $wgTwlRegistrationDays * 24 * 3600;
-				if ( $globalUser->getGlobalEditCount() >= $wgTwlEditCount && $accountAge >= $minimumAge ) {
-					EchoEvent::create( [
-						'type' => 'twl-eligible',
-						'agent' => $user,
-					] );
-
-					// Set the twl-notified preference globally, so we'll know not to notify this user again
-					$prefsFactory = MediaWikiServices::getInstance()->getPreferencesFactory();
-					'@phan-var \GlobalPreferences\GlobalPreferencesFactory $prefsFactory';
-					$prefsFactory->setUser( $user );
-					$prefs = $prefsFactory->getGlobalPreferencesValues( true );
-					$prefs['twl-notified'] = 1;
-					$prefsFactory->setGlobalPreferences( $prefs, RequestContext::getMain() );
-				}
-			} );
+			self::maybeSendNotification( $user );
 		}
+	}
+
+	/**
+	 * Shared implementation for PageContentSaveComplete and PageSaveComplete
+	 *
+	 * @param User $user
+	 */
+	private static function maybeSendNotification( User $user ) {
+		// Send a notification if the user has at least $wgTwlEditCount edits and their account
+		// is at least $wgTwlRegistrationDays days old
+		DeferredUpdates::addCallableUpdate( function () use ( $user ) {
+			global $wgTwlEditCount, $wgTwlRegistrationDays;
+
+			// If we've already notified this user, don't notify them again
+			if ( $user->getOption( 'twl-notified' ) ) {
+				return;
+			}
+
+			$globalUser = CentralAuthUser::getInstance( $user );
+			if ( !$globalUser->isAttached() ) {
+				return;
+			}
+			$accountAge = (int)wfTimestamp( TS_UNIX ) -
+				(int)wfTimestamp( TS_UNIX, $globalUser->getRegistration() );
+			$minimumAge = $wgTwlRegistrationDays * 24 * 3600;
+			if ( $globalUser->getGlobalEditCount() >= $wgTwlEditCount && $accountAge >= $minimumAge ) {
+				EchoEvent::create( [
+					'type' => 'twl-eligible',
+					'agent' => $user,
+				] );
+
+				// Set the twl-notified preference globally, so we'll know not to notify this user again
+				$prefsFactory = MediaWikiServices::getInstance()->getPreferencesFactory();
+				'@phan-var \GlobalPreferences\GlobalPreferencesFactory $prefsFactory';
+				$prefsFactory->setUser( $user );
+				$prefs = $prefsFactory->getGlobalPreferencesValues( true );
+				$prefs['twl-notified'] = 1;
+				$prefsFactory->setGlobalPreferences( $prefs, RequestContext::getMain() );
+			}
+		} );
 	}
 }
